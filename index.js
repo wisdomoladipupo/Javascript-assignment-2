0709c1c67ed39c7efa6b6d5dc1fbe985717eced1
wisdom oladipupo <wisbel71@gmail.com>
2024-11-08 19:43:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+ const container = document.getElementById("container")
+
+
+ const loremText = document.createElement("p")
+
+ loremText.textContent = ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, itaque
+        quia dolore alias ad suscipit voluptate nemo inventore nam,
+        exercitationem natus enim, ipsa consequuntur quidem ullam temporibus
+        consectetur voluptates aut!`
+
+        
